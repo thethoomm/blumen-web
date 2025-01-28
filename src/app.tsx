2e42df708d63c6abc8822a8fedf524227f4bf2eb
thethoomm <thomas.santos02209@gmail.com>
2025-01-28 15:26:39 -0300

    feat: add design tokens

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,21 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-500">app</h1>
+      <div className="text-2xl">
+      <h1 className="font-pacifico">pacifico</h1>
+      <h1 className="font-eb-garamond">eb garamond</h1>
+      <h1 className="font-raleway">raleway</h1>
+      <h1 className="font-lato">lato</h1>
+      <h1 className="font-inter">inter</h1>
+      </div>
+
+      <div className="w-[100px] h-[100px] bg-primary-lunar"></div>
+      <div className="w-[100px] h-[100px] bg-primary-avacado"></div>
+      <div className="w-[100px] h-[100px] bg-primary-accent"></div>
+      <div className="w-[100px] h-[100px] bg-secondary"></div>
+      <div className="w-[100px] h-[100px] bg-athena-gray"></div>
+      <div className="w-[100px] h-[100px] bg-white"></div>
+      <div className="w-[100px] h-[100px] bg-black"></div>
     </div>
-  )
+  );
 }
