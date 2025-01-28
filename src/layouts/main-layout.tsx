316dabafdfb61acb35d70e7092e42d96b0f4ed3f
thethoomm <thomas.santos02209@gmail.com>
2025-01-28 17:20:17 -0300

    feat: create footer

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router";
 import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
 export default function MainLayout() {
     return (
         <div>
             <Navbar />
-            <main>
+            <main className="min-h-screen">
                 <Outlet />
             </main>
+            <Footer />
         </div>
     )
 }
