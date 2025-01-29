cc12940723dd9e2e67f9cd2c756298ea6cf5b1d0
thethoomm <thomas.santos02209@gmail.com>
2025-01-29 09:03:54 -0300

    feat: create not found page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import MainLayout from "@/layouts/main-layout";
 import Home from "@/pages/home";
 import PlantDetails from "@/pages/plant-details";
+import NotFound from "@/pages/not-found";
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/products/:plantId" element={<PlantDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
